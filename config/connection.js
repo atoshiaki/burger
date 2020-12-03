d0d689d53801ebbbbfafe4e6484159c45ad5c51d
atoshiaki <66647837+atoshiaki@users.noreply.github.com>
2020-12-03 01:30:51 -0800

    Add files via upload

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,15 @@
 const mysql = require("mysql");
-connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-});
+
+if (process.env.JAWSBD_URL){
+    connection = mysql.createConnection(process.env.JAWSBD_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "burgers_db"
+    });
+}
 connection.connect(function (err) {
     if (err) {
         console.error("error:" + err.stack);
