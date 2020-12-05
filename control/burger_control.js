30c35fb8e1f5ffcd7081d9ac47f3d1fdf474508f
atoshiaki <66647837+atoshiaki@users.noreply.github.com>
2020-12-04 21:32:16 -0800

    Add files via upload

--- control/burger_control.js
+++ control/burger_control.js
@@ -10,7 +10,7 @@ router.get("/", function(req, res){
         console.log(hdlbrs)
         res.render("index", hdlbrs);
     });
-    router.post("api/burgers", function(req, res){
+    router.post("/api/burgers", function(req, res){
         burger.insertOne(
             ["burgers_name", "devoured"],
             [req.body.burger_name, req.body.devoured],
@@ -19,7 +19,7 @@ router.get("/", function(req, res){
             }
         );
     });
-    router.put("/api/burgers/: id", function(req, res){
+    router.put("/api/burgers/:id", function(req, res){
         var condition = "id =" + req.params.id;
         burger.update({ devoured: req.body.devoured}, condition, function (result) {
             if ((result. changeRows === 0)) {
--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 var express = require("express");
-var bP = require("body-parser");
 var exphbs = require("express-handlebars");
 var app = express();
 var PORT = process.env.PORT || 5000;
 
 app.use(express.static("public"));
-app.use(bP.urlencoded({ extended: true }));
-app.use(bP.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", + __dirname + "handlebars");
+app.set("view engine", "handlebars");
 
 var routes = require("./control/burger_control.js");
 app.use(routes);
